7805b883c6659067bee5382da754a50abfaee397
Vahid Eyorokon <veyorokon@gmail.com>
2025-04-25 19:22:08 -0400

    init

--- finetrainers/trainer/e2v_trainer/__init__.py
+++
@@ -1,6 +0,0 @@
-"""E2V trainer module for elements-to-video training."""
-
-from .trainer import E2VTrainer
-from .config import E2VConfig, E2VLowRankConfig, E2VFullRankConfig
-
-__all__ = ["E2VTrainer", "E2VConfig", "E2VLowRankConfig", "E2VFullRankConfig"]
