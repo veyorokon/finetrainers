cd1cf54ba24e8da14e4e374befbbfc87e1bdb262
Vahid Eyorokon <veyorokon@gmail.com>
2025-04-25 21:10:03 -0400

    removed old ref

--- finetrainers/__init__.py
+++ finetrainers/__init__.py
@@ -2,7 +2,6 @@ from .args import BaseArgs
 from .config import ModelType, TrainingType
 from .logging import get_logger
 from .models import ModelSpecification
-from .trainer import ControlTrainer, SFTTrainer, E2VTrainer, ReferenceTrainer
-
+from .trainer import ControlTrainer, ReferenceTrainer, SFTTrainer
 
 __version__ = "0.1.0"
